d2324be96d1b96b93abe7aaa55bcd6337ca4f5b1
Sohail <alisohail2448@gmail.com>
2022-03-05 16:32:33 -0800

    done

--- backend/app.js
+++ backend/app.js
@@ -39,9 +39,23 @@ app.get("/signup", (req, res) => {
 
 //HEROKU
 
-if(process.env.NODE_ENV == "production"){
-  app.use(express.static("frontend/build"))
-}
+// if(process.env.NODE_ENV == "production"){
+//   app.use(express.static("frontend/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) =>{
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", 'index.html'));
+//   })
+// }
+
+  const path = require("path");
+  if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"backend/frontend/build")));
+    app.get("*", (req,res)=>{
+      res.sendFile(path.resolve(__dirname, 'frontend','build','index.html'))
+    })
+  }
+
+
 
 
 
